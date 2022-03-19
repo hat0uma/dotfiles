9b877fa635fb0b2ede9e0efa3435135b5dc8cef4
rikuma-t <55551571+rikuma-t@users.noreply.github.com>
2022-03-19 13:12:19 +0900

    use filetype.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -122,7 +122,9 @@ vim.o.mousefocus = false
 vim.g.did_install_default_menus = 1
 vim.g.did_install_syntax_menu = 1
 -- vim.g.did_indent_on = 1
--- vim.g.did_load_filetypes = 1
+vim.g.did_load_filetypes = 0
+vim.g.do_filetype_lua = 1
+
 -- vim.g.did_load_ftplugin = 1
 vim.g.loaded_2html_plugin = 1
 vim.g.loaded_gzip = 1
