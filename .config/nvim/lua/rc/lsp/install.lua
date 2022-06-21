c380155b473e18bb1d3ddb4334f502d88a6ab6f8
rikuma-t <55551571+rikuma-t@users.noreply.github.com>
2022-06-22 01:16:04 +0900

    update

--- .config/nvim/lua/rc/lsp/install.lua
+++ .config/nvim/lua/rc/lsp/install.lua
@@ -1,54 +1,9 @@
 -- install_servers
--- https://github.com/williamboman/nvim-lsp-installer/issues/179#issuecomment-946674767
-
-local function install_servers_sync(servers)
-  local process = require("nvim-lsp-installer.process")
-  local lsp_installer_servers = require("nvim-lsp-installer.servers")
-
-  local completed = 0
-  local server_count = 0
-  for name,info in pairs(servers) do
-
-    server_count = server_count + 1
-    local ok, server = lsp_installer_servers.get_server(name)
-    if not ok then
-      print("Could not get server info " .. server.name)
-      completed = completed + 1
-      goto continue
-    end
-
-    if server:is_installed() then
-      print("Server " .. server.name .. " already installed.")
-      completed = completed + 1
-      goto continue
-    end
-
-    local version = info.version
-    server:install_attached({
-      stdio_sink = process.simple_sink(),
-      requested_server_version = version },
-      function (success)
-        if not success then
-          print("Server " .. server.name .. " failed to install.")
-        else
-          print("Server " .. server.name .. " install success.")
-        end
-        completed = completed + 1
-      end
-    )
-
-    ::continue::
-  end
-
-  -- wait all server install
-  vim.wait(10000, function() return completed >= server_count end )
-end
-
 local function install_configured_servers_sync()
-  install_servers_sync(require("rc.lsp").configured_servers.auto)
+  local servers = vim.tbl_keys(require("rc.lsp").configured_servers.auto)
+  vim.cmd("LspInstall --sync " .. table.concat(servers, " "))
 end
 
 return {
-  install_configured_servers_sync = install_configured_servers_sync
+  install_configured_servers_sync = install_configured_servers_sync,
 }
-
