34be52e1f61ecefbe10c9f6189116d52968ef836
rikuma-t <55551571+rikuma-t@users.noreply.github.com>
2022-07-28 00:33:47 +0900

    update

--- win/profile.ps1
+++ win/profile.ps1
@@ -106,64 +106,6 @@ function cut()
     }
 }
 
-# https://gist.github.com/aroben/5542538
-function pstree
-{
-    $ProcessesById = @{}
-    foreach ($Process in (Get-WMIObject -Class Win32_Process))
-    {
-        $ProcessesById[$Process.ProcessId] = $Process
-    }
-
-    $ProcessesWithoutParents = @()
-    $ProcessesByParent = @{}
-    foreach ($Pair in $ProcessesById.GetEnumerator())
-    {
-        $Process = $Pair.Value
-
-        if (($Process.ParentProcessId -eq 0) -or !$ProcessesById.ContainsKey($Process.ParentProcessId))
-        {
-            $ProcessesWithoutParents += $Process
-            continue
-        }
-
-        if (!$ProcessesByParent.ContainsKey($Process.ParentProcessId))
-        {
-            $ProcessesByParent[$Process.ParentProcessId] = @()
-        }
-        $Siblings = $ProcessesByParent[$Process.ParentProcessId]
-        $Siblings += $Process
-        $ProcessesByParent[$Process.ParentProcessId] = $Siblings
-    }
-
-    function Show-ProcessTree([UInt32]$ProcessId, $IndentLevel)
-    {
-        $Process = $ProcessesById[$ProcessId]
-        $Indent = " " * $IndentLevel
-        if ($Process.CommandLine)
-        {
-            $Description = $Process.CommandLine
-        } else
-        {
-            $Description = $Process.Caption
-        }
-
-        Write-Output ("{0,6}{1} {2}" -f $Process.ProcessId, $Indent, $Description)
-        foreach ($Child in ($ProcessesByParent[$ProcessId] | Sort-Object CreationDate))
-        {
-            Show-ProcessTree $Child.ProcessId ($IndentLevel + 4)
-        }
-    }
-
-    Write-Output ("{0,6} {1}" -f "PID", "Command Line")
-    Write-Output ("{0,6} {1}" -f "---", "------------")
-
-    foreach ($Process in ($ProcessesWithoutParents | Sort-Object CreationDate))
-    {
-        Show-ProcessTree $Process.ProcessId 0
-    }
-}
-
 function find-file($name)
 {
     get-childitem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | foreach-object {
