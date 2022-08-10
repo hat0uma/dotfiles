7687a2d0d1d2a82aae3aed29d76f5b2004e06274
rikuma-t <55551571+rikuma-t@users.noreply.github.com>
2022-08-10 23:54:59 +0900

    update

--- win/profile.ps1
+++ win/profile.ps1
@@ -14,17 +14,11 @@ $MaximumHistoryCount = 10000;
 
 # aliases and functions
 Set-Alias -Name ll -Value Get-ChildItem
-Set-Alias -Name editor -Value nvim
 Remove-Alias -Force -Name sp
 
 if ( Test-Path env:NVIM )
 {
     . _nvim_hooks.ps1
-}
-
-function edit($file)
-{ 
-    editor $file
 }
 
 function open($file)
@@ -42,22 +36,6 @@ function pkill($name)
     get-process $name -ErrorAction SilentlyContinue | stop-process
 }
 
-function pgrep($name)
-{
-    get-process $name
-}
-
-function touch($file)
-{
-    if ( Test-Path $file )
-    {
-        Set-FileTime $file
-    } else
-    {
-        New-Item $file -type file
-    }
-}
-
 function ln($target, $link)
 {
     New-Item -ItemType SymbolicLink -Path $link -Value $target
@@ -68,37 +46,3 @@ function df
     get-volume
 }
 
-function grep($regex, $dir)
-{
-    if ( $dir )
-    {
-        get-childitem $dir | select-string $regex
-        return
-    }
-    $input | select-string $regex
-}
-
-function which($name)
-{
-    Get-Command $name | Select-Object -ExpandProperty Definition
-}
-
-function cut()
-{
-    foreach ($part in $input)
-    {
-        $line = $part.ToString();
-        $MaxLength = [System.Math]::Min(200, $line.Length)
-        $line.subString(0, $MaxLength)
-    }
-}
-
-function find-file($name)
-{
-    get-childitem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | foreach-object {
-        write-output($PSItem.FullName)
-    }
-}
-
-set-alias find find-file
-set-alias find-name find-file
