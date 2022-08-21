ad7014fd67967c294d8c655d38e1a47ddee18a1e
rikuma-t <55551571+rikuma-t@users.noreply.github.com>
2022-08-22 02:20:59 +0900

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -39,6 +39,7 @@ return {
   launch_menu = launch_menu,
   use_ime = true,
   enable_tab_bar = true,
+  use_fancy_tab_bar = false,
   hide_tab_bar_if_only_one_tab = true,
   tab_bar_at_bottom = false,
   window_decorations = "NONE",
