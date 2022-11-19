1a7790e2fcdc719bc8a4c2968a11ea61b3c8c24d
rikuma-t <55551571+rikuma-t@users.noreply.github.com>
2022-11-19 23:08:13 +0900

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,20 +1,28 @@
 local wezterm = require "wezterm"
 local act = wezterm.action
+local is_windows = wezterm.target_triple == "x86_64-pc-windows-msvc"
 
-local terminal_on_neovim = {}
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+local terminal_on_neovim
+if is_windows then
   terminal_on_neovim = { os.getenv "UserProfile" .. "/dotfiles/win/nvim_terminal.cmd" }
 else
   terminal_on_neovim = { os.getenv "HOME" .. "/dotfiles/scripts/nvim_terminal.sh" }
 end
 
 local launch_menu = {}
-table.insert(launch_menu, { label = "neovim", args = { "nvim" } })
-table.insert(launch_menu, { label = "neovim-terminal", args = terminal_on_neovim })
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+if is_windows then
   table.insert(launch_menu, { label = "pwsh", args = { "pwsh", "-NoLogo" } })
 else
   table.insert(launch_menu, { label = "zsh", args = { "zsh", "-l" } })
+end
+table.insert(launch_menu, { label = "neovim", args = { "nvim" } })
+table.insert(launch_menu, { label = "neovim-terminal", args = terminal_on_neovim })
+
+local default_prog
+if is_windows then
+  default_prog = { "pwsh", "-NoLogo" }
+else
+  default_prog = { "zsh", "-l" }
 end
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
@@ -39,15 +47,24 @@ return {
   -- xim_im_name = "fcitx",
   font = wezterm.font_with_fallback { "Sarasa Term J Nerd Font", "Twemoji Mozilla" },
   harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-  default_prog = terminal_on_neovim,
+  -- default_prog = terminal_on_neovim,
+  default_prog = default_prog,
+  leader = { key = "b", mods = "CTRL" },
   keys = {
     { key = "q", mods = "ALT", action = act.CloseCurrentPane { confirm = false } },
     { key = "0", mods = "ALT", action = act.QuitApplication },
-    { key = "Enter", mods = "ALT", action = wezterm.action.ShowLauncher },
-    { key = "f", mods = "ALT", action = wezterm.action.ToggleFullScreen },
+    { key = "Enter", mods = "ALT", action = act.ShowLauncher },
+    { key = "f", mods = "ALT", action = act.ToggleFullScreen },
     { key = "1", mods = "ALT", action = act.ActivateTab(0) },
     { key = "2", mods = "ALT", action = act.ActivateTab(1) },
     { key = "3", mods = "ALT", action = act.ActivateTab(2) },
     { key = "4", mods = "ALT", action = act.ActivateTab(3) },
+    { key = "\\", mods = "ALT", action = act.SplitHorizontal {} },
+    { key = "-", mods = "ALT", action = act.SplitVertical {} },
+    { key = "r", mods = "LEADER", action = act.ReloadConfiguration },
+    { key = "h", mods = "LEADER", action = act { ActivatePaneDirection = "Left" } },
+    { key = "j", mods = "LEADER", action = act { ActivatePaneDirection = "Down" } },
+    { key = "k", mods = "LEADER", action = act { ActivatePaneDirection = "Up" } },
+    { key = "l", mods = "LEADER", action = act { ActivatePaneDirection = "Right" } },
   },
 }
