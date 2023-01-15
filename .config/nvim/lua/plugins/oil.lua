9c07ba803d56d17c280e8f81000d11738e438230
rikuma-t <55551571+rikuma-t@users.noreply.github.com>
2023-01-16 00:56:18 +0900

    update

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -11,15 +11,31 @@ local function open()
     on_edit.filename = ""
     on_edit.dir = vim.loop.cwd()
   end
-  -- require("oil").open_float(on_edit.dir)
   -- vim.cmd.tabnew()
-  require("oil").open(on_edit.dir)
+  -- require("oil").open(on_edit.dir)
+  require("oil").open_float(on_edit.dir)
+end
+
+local function select_for_float(base)
+  return {
+    desc = base.desc .. " for float",
+    callback = function()
+      local oil = require "oil"
+      local entry = oil.get_cursor_entry()
+      if entry and entry.type == "directory" then
+        local current = oil.get_current_dir()
+        oil.close()
+        oil.open(current)
+      end
+      base.callback()
+    end,
+  }
 end
 
 return {
   "stevearc/oil.nvim",
   init = function()
-    vim.keymap.set("n", "-", open, { desc = "Open parent directory" })
+    vim.keymap.set("n", "<leader>e", open, { desc = "Open current file directory" })
   end,
   config = function()
     require("oil").setup {
@@ -33,13 +49,14 @@ return {
       keymaps = {
         ["g?"] = require("oil.actions").show_help,
         ["q"] = require("oil.actions").close,
-        ["-"] = require("oil.actions").close,
+        ["<leader>e"] = require("oil.actions").close,
 
         ["L"] = require("oil.actions").select,
         ["H"] = require("oil.actions").parent,
-        ["S"] = require("oil.actions").select_split,
-        ["V"] = require("oil.actions").select_vsplit,
+        ["gv"] = select_for_float(require("oil.actions").select_vsplit),
+        ["gs"] = select_for_float(require("oil.actions").select_split),
         ["g."] = require("oil.actions").toggle_hidden,
+        ["gp"] = require("oil.actions").preview,
       },
       float = {
         padding = 2,
