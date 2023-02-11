df384cca152e7fdd6d1d94e619f6635b6ea11d48
rikuma-t <55551571+rikuma-t@users.noreply.github.com>
2023-02-11 17:13:52 +0900

    update

--- .config/nvim/lua/rc/terminal/bin/_nvim_hooks.zsh
+++ .config/nvim/lua/rc/terminal/bin/_nvim_hooks.zsh
@@ -1,7 +1,46 @@
 #!/usr/bin/env zsh
-_nvim_autocd(){
+
+# change terminal buffer pwd
+function _nvim_autocd(){
     nvim --server "$PARENT_NVIM_ADDRESS" --remote-send "<Cmd>lcd $PWD<CR>"
 }
 
 add-zsh-hook chpwd _nvim_autocd
 
+# notify long command finished
+function _notify (){
+    local last_command="$1"
+    local last_command_status="$2"
+    local time_elapsed="$3"
+
+    local level msg
+    if (( last_command_status == 0 )); then
+        level="info"
+        msg="$_nvim_last_command completed"
+    else
+        level="error"
+        msg="$_nvim_last_command failed"
+    fi
+
+    nvim --server "$PARENT_NVIM_ADDRESS" --remote-send "<Cmd>lua vim.notify('$msg','$level')<CR>"
+}
+
+function _notify_before_command(){
+    declare -g _nvim_last_command="$1"
+    declare -g _nvim_command_starttime="$EPOCHSECONDS"
+}
+
+function _notify_after_command(){
+    local command_status="$?"
+    if [[ -n $_nvim_last_command && -n $_nvim_command_starttime ]]; then
+        local time_elapsed=$(( EPOCHSECONDS - _nvim_command_starttime ))
+        local threshold="${NVIM_NOTIFY_THRESHOLD:-2}"
+        if ((  time_elapsed >= threshold  )); then
+            _notify "$_nvim_last_command" "$command_status" "$time_elapsed"
+        fi
+    fi
+    unset zsh_notify_last_command zsh_notify_start_time
+}
+
+add-zsh-hook preexec _notify_before_command
+add-zsh-hook precmd _notify_after_command
