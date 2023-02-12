dd1dbb52c42a2ff6142c98f3a2efef11cd04baad
rikuma-t <55551571+rikuma-t@users.noreply.github.com>
2023-02-13 00:15:25 +0900

    update

--- .config/nvim/lua/rc/utils.lua
+++ .config/nvim/lua/rc/utils.lua
@@ -4,6 +4,9 @@ local M = {}
 
 function M.accessable_path(cwd, file)
   --- @type string
+  if cwd == nil then
+    return file
+  end
   local retpath = Path:new({ cwd, file }):absolute()
   if not vim.loop.fs_access(retpath, "R", nil) then
     retpath = file
