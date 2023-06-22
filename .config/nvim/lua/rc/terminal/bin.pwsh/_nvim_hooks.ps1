de98e89d94e54c8829f16c2147b5d23af98fa7dc
rikuma-t <55551571+rikuma-t@users.noreply.github.com>
2023-06-22 23:47:42 +0900

    update

--- .config/nvim/lua/rc/terminal/bin.pwsh/_nvim_hooks.ps1
+++ .config/nvim/lua/rc/terminal/bin.pwsh/_nvim_hooks.ps1
@@ -6,7 +6,7 @@ function _cd_nvim_hook()
     Invoke-Expression "Set-Location $args"
     if( "$PWD" -ne "$Script:OLDPWD" )
     {
-        $(nvim -u NONE -i NORC --server $env:PARENT_NVIM_ADDRESS --remote-send "<Cmd>lua require('rc.terminal.dir').notify_cwd_changed([[$PWD]])<CR>")
+        nvim -u NONE -i NORC --server $env:PARENT_NVIM_ADDRESS --remote-send "<Cmd>lua require('rc.terminal.dir').notify_cwd_changed([[$PWD]])<CR>" | Out-Null
         $Script:OLDPWD=$PWD
     }
 }
