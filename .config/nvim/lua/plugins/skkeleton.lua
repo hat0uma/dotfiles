2085012adf501079c9bad37015bcda6288e019bb
hat0uma <55551571+hat0uma@users.noreply.github.com>
2024-06-27 02:22:33 +0900

    update

--- .config/nvim/lua/plugins/skkeleton.lua
+++ .config/nvim/lua/plugins/skkeleton.lua
@@ -29,5 +29,5 @@ return {
   end,
   dependencies = { "denops.vim" },
   keys = { { "<C-j>", mode = { "i", "c" } } },
-  build = require("plugins.denops").cache "skkeleton",
+  -- build = require("plugins.denops").cache "skkeleton",
 }
