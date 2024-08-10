bc832492fb7017ca44aa6d8511f547357ea5e15c
hat0uma <55551571+hat0uma@users.noreply.github.com>
2024-08-10 17:54:59 +0900

    update

--- scripts/install_neovim.sh
+++ scripts/install_neovim.sh
@@ -4,23 +4,26 @@ version=${1:-"master"}
 neovim_tmp_dir=${2:-"/tmp/nvim"}
 echo "*** start install neovim@${version} ***"
 
-if type yay &> /dev/null; then
-    yay -S --noconfirm base-devel cmake unzip ninja tree-sitter curl
-elif type apt-get &> /dev/null; then
-    sudo apt-get install -y ninja-build gettext cmake unzip curl build-essential
+if type yay &>/dev/null; then
+	yay -S --noconfirm base-devel cmake unzip ninja tree-sitter curl
+elif type apt-get &>/dev/null; then
+	sudo apt-get install -y ninja-build gettext cmake unzip curl build-essential
 else
-    echo "unknown package manager."
-    exit -1
+	echo "unknown package manager."
+	exit 1
 fi
 
 # checkout
 if [[ ! -d "$neovim_tmp_dir" ]]; then
-    git clone https://github.com/neovim/neovim "$neovim_tmp_dir" || { echo "failed to clone."; exit 1; }
-else
-    git pull --all
+	git clone https://github.com/neovim/neovim "$neovim_tmp_dir" || {
+		echo "failed to clone."
+		exit 1
+	}
 fi
+
 cd "$neovim_tmp_dir"
 git checkout "$version"
+git pull
 
 # build & install
 make CMAKE_BUILD_TYPE=RelWithDebInfo -j"$(nproc)"
