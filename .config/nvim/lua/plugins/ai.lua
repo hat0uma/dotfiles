4c5ad73d855a944aae38d163323121936cdf9517
hat0uma <55551571+hat0uma@users.noreply.github.com>
2025-01-05 22:42:39 +0900

    feat: add avante.nvim plugin with dependencies
    Added avante.nvim plugin with necessary dependencies and configuration.
    Updated CopilotChat.nvim branch to main. Modified vim.yml to include rc
    global variable.

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,7 +1,52 @@
 return {
   {
+    "yetone/avante.nvim",
+    config = function()
+      require("avante").setup({
+        provider = "copilot",
+      })
+    end,
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      -- "hrsh7th/nvim-cmp",
+      "nvim-tree/nvim-web-devicons",
+      "zbirenbaum/copilot.lua",
+      {
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = { file_types = { "markdown", "Avante" } },
+        ft = {
+          -- "markdown",
+          "Avante",
+        },
+      },
+    },
+    event = "VeryLazy",
+    build = rc.sys.is_windows and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" or "make",
+    cond = function()
+      return vim.env.ENABLE_NVIM_AI_PLUGINS == "1"
+    end,
+  },
+  {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
+    branch = "main",
     dependencies = {
       { "zbirenbaum/copilot.lua" },
       { "nvim-lua/plenary.nvim" },
