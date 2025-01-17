1f3435ce9146e5eb34197804542e6390ea4b0b72
hat0uma <55551571+hat0uma@users.noreply.github.com>
2025-01-17 20:33:41 +0900

    update

---
+++ .config/nvim/lua/rc/toys/odbc/deferrable.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+---@alias Deferred fun(...:any)...
+---@alias Defer fun( f: Deferred, ...:any)
+
+---Call in deferrable context
+---@param target_fn fun( defer:Defer )
+function M.run(target_fn)
+  local deferred_list = {}
+
+  ---@type Defer
+  local function defer(deferred, ...)
+    local args = { ... }
+    if #args == 0 then
+      table.insert(deferred_list, deferred)
+      return
+    end
+
+    table.insert(deferred_list, function()
+      deferred(unpack(args))
+    end)
+  end
+
+  -- Run target functions
+  local ok, err = xpcall(target_fn, debug.traceback, defer) --- @type boolean,any
+
+  -- Do deferred functions
+  for _ = 1, #deferred_list do
+    local f = table.remove(deferred_list)
+    pcall(f)
+  end
+
+  -- notify errors
+  if not ok then
+    vim.notify(err, vim.log.levels.ERROR)
+  end
+end
+
+return M
--- .config/nvim/lua/rc/toys/odbc/test.lua
+++ .config/nvim/lua/rc/toys/odbc/test.lua
@@ -1,83 +1,76 @@
 local M = {}
 
+local deferrable = require("rc.toys.odbc.deferrable")
 local odbc = require("rc.toys.odbc")
 
--- https://stackoverflow.com/questions/49506048/odbc-xlsx-driver-connectionstring-without-header/49555650#49555650
-local driver = "Microsoft Excel Driver (*.xls, *.xlsx, *.xlsm, *.xlsb)"
-local excel_file = vim.fs.normalize("~/work/test.xlsx")
-local conn_str = string.format("Driver={%s};DBQ=%s;ReadOnly=1", driver, excel_file)
+deferrable.run(function(defer)
+  -- https://stackoverflow.com/questions/49506048/odbc-xlsx-driver-connectionstring-without-header/49555650#49555650
+  local driver = "Microsoft Excel Driver (*.xls, *.xlsx, *.xlsm, *.xlsb)"
+  local excel_file = vim.fs.normalize("~/work/test.xlsx")
+  local conn_str = string.format("Driver={%s};DBQ=%s;ReadOnly=1", driver, excel_file)
 
-local err
+  local err
 
--- Create Env
-local env
-env, err = odbc.new_env()
-if not env then
-  error(err)
-end
+  -- Create Env
+  local env
+  env, err = odbc.new_env()
+  if not env then
+    error(err)
+  end
+  defer(env.close, env)
 
--- Set ODBC version
-local ok
-ok, err = env:set_version(3)
-if not ok then
-  env:close()
-  error(err)
-end
-
--- Create new connection
-local conn
-conn, err = env:new_connection(conn_str)
-if not conn then
-  env:close()
-  error(err)
-end
-
--- Open connection
-local out_conn_str
-out_conn_str, err = conn:open()
-if not out_conn_str then
-  conn:close()
-  env:close()
-  error(err)
-end
-
-print(string.format("Connected with [%s]", out_conn_str))
-
--- Execute query
-local query = "SELECT * FROM [Sheet1$]"
-local cur
-cur, err = conn:execute(query)
-if not cur then
-  conn:disconnect()
-  conn:close()
-  env:close()
-  error(err)
-end
-
-print("Executed SQL query:", query)
-
--- Fetch rows
-local i = 1
-while true do
-  local row
-  row, err = cur:fetch()
-  if not row then
-    break
+  -- Set ODBC version
+  local ok
+  ok, err = env:set_version(3)
+  if not ok then
+    error(err)
   end
 
-  -- Print result
-  print("row", i, table.concat(row, "\t"))
-  i = i + 1
-end
+  -- Create new connection
+  local conn
+  conn, err = env:new_connection(conn_str)
+  if not conn then
+    error(err)
+  end
+  defer(conn.close, conn)
 
-cur:close()
-conn:disconnect()
-conn:close()
-env:close()
+  -- Open connection
+  local out_conn_str
+  out_conn_str, err = conn:open()
+  if not out_conn_str then
+    error(err)
+  end
+  defer(conn.disconnect, conn)
 
-if err then
-  error(err)
-end
-print("Disconnected and cleaned up ODBC handles")
+  print(string.format("Connected with [%s]", out_conn_str))
+
+  -- Execute query
+  local query = "SELECT * FROM [Sheet1$]"
+  local cur
+  cur, err = conn:execute(query)
+  if not cur then
+    error(err)
+  end
+  defer(cur.close, cur)
+
+  print("Executed SQL query:", query)
+
+  -- Fetch rows
+  local i = 1
+  while true do
+    local row
+    row, err = cur:fetch()
+    if err then
+      error(err)
+    end
+    if not row then
+      break
+    end
+
+    -- Print result
+    print("row", i, table.concat(row, "\t"))
+    i = i + 1
+  end
+end)
 
 return M
