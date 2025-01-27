8c1a109ebca239a43239854aa50aa3e5c3e9505d
hat0uma <55551571+hat0uma@users.noreply.github.com>
2025-01-27 12:39:54 +0900

    update

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -8,6 +8,7 @@ return {
     vim.keymap.set("n", "zM", function()
       require("ufo").closeAllFolds()
     end)
+    vim.keymap.set("n", "<Tab>", "za") -- toggle folding
   end,
   config = function()
     require("ufo").setup({
@@ -15,34 +16,22 @@ return {
       provider_selector = function(bufnr, filetype, buftype)
         return { "treesitter", "indent" }
       end,
-      fold_virt_text_handler = function(virt_text, lnum, end_lnum, width, truncate)
+      fold_virt_text_handler = function(virt_text, lnum, end_lnum, width, truncate, ctx)
         local result = {}
-        local _end = end_lnum - 1
-        local final_text = vim.trim(vim.api.nvim_buf_get_text(0, _end, 0, _end, -1, {})[1])
-        local suffix = final_text:format(end_lnum - lnum)
-        local suffix_width = vim.fn.strdisplaywidth(suffix)
-        local target_width = width - suffix_width
-        local cur_width = 0
-        for _, chunk in ipairs(virt_text) do
-          local chunk_text = chunk[1]
-          local chunk_width = vim.fn.strdisplaywidth(chunk_text)
-          if target_width > cur_width + chunk_width then
+        local first_line = ctx.get_fold_virt_text(lnum)
+        local last_line = ctx.get_fold_virt_text(end_lnum)
+        vim.list_extend(result, first_line)
+        vim.list_extend(result, {
+          { " ", "UfoPreviewCursorLine" },
+          { " ... ", "UfoFoldedEllipsis" },
+          { " ", "UfoPreviewCursorLine" },
+        })
+        for _, chunk in ipairs(last_line) do
+          if vim.trim(chunk[1]):len() ~= 0 then
             table.insert(result, chunk)
-          else
-            chunk_text = truncate(chunk_text, target_width - cur_width)
-            local hl_group = chunk[2]
-            table.insert(result, { chunk_text, hl_group })
-            chunk_width = vim.fn.strdisplaywidth(chunk_text)
-            -- str width returned from truncate() may less than 2nd argument, need padding
-            if cur_width + chunk_width < target_width then
-              suffix = suffix .. (" "):rep(target_width - cur_width - chunk_width)
-            end
-            break
           end
-          cur_width = cur_width + chunk_width
         end
-        table.insert(result, { " ⋯ ", "NonText" })
-        table.insert(result, { suffix, "TSPunctBracket" })
+
         return result
       end,
     })
