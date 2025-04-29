750503233b26f0f488b34248f096584f17c603bc
hat0uma <55551571+hat0uma@users.noreply.github.com>
2025-04-29 21:35:29 +0900

    update

--- .config/nvim/lua/rc/toys/odbc/init.lua
+++ .config/nvim/lua/rc/toys/odbc/init.lua
@@ -1,10 +1,11 @@
 local M = {}
-local ODBCEnv = require("rc.toys.odbc.env")
-local sql = require("rc.toys.odbc.sql")
 
 --- Create new ODBC.Env object
 ---@return ODBC.Env? env, string? err_msg, string? err_name
 function M.new_env()
+  local ODBCEnv = require("rc.toys.odbc.env")
+  local sql = require("rc.toys.odbc.sql")
+
   local henv, err_msg, err_name = sql.alloc_handle(sql.HandleType.SQL_HANDLE_ENV)
   if not henv then
     return nil, err_msg, err_name
