9f3d0f4fd41c6030d3c1b3613cbee585d46ee55a
hat0uma <55551571+hat0uma@users.noreply.github.com>
2026-09-22 21:49:19 +0900

    update

--- .config/aconfmgr/33-packages-sdr.sh
+++ .config/aconfmgr/33-packages-sdr.sh
@@ -1,2 +1,3 @@
 AddPackage gnuradio           # Signal processing runtime and signal processing software development toolkit
 AddPackage gnuradio-companion # Signal processing runtime and signal processing software development toolkit (GUI)
+AddPackage urh                # Universal Radio Hacker: investigate wireless protocols like a boss
--- .config/nvim/lua/rc/terminal/dir.lua
+++ .config/nvim/lua/rc/terminal/dir.lua
@@ -4,23 +4,38 @@ local M = {}
 
 --- Setup the terminal directory feature.
 function M.setup()
-  vim.api.nvim_create_autocmd({ "TermRequest" }, {
+  -- vim.api.nvim_create_autocmd({ "TermRequest" }, {
+  --   desc = "Handles OSC 7 dir change requests",
+  --   callback = function(ev)
+  --     local uri = string.match(ev.data.sequence, "\027]7;([^%c]+)")
+  --     if not uri then
+  --       return
+  --     end
+  --
+  --     local dir = vim.uri_to_fname(uri)
+  --     if vim.fn.isdirectory(dir) == 0 then
+  --       vim.notify(string.format("invalid dir: %s, uri: %s", dir, uri))
+  --       return
+  --     end
+  --
+  --     vim.b[ev.buf].osc7_dir = dir
+  --     if vim.api.nvim_get_current_buf() == ev.buf then
+  --       vim.cmd.lcd(dir)
+  --     end
+  --   end,
+  -- })
+  vim.api.nvim_create_autocmd("TermRequest", {
     desc = "Handles OSC 7 dir change requests",
     callback = function(ev)
-      local uri = string.match(ev.data.sequence, "\027]7;([^%c]+)")
-      if not uri then
-        return
-      end
-
-      local dir = vim.uri_to_fname(uri)
-      if vim.fn.isdirectory(dir) == 0 then
-        vim.notify("invalid dir: " .. dir)
-        return
-      end
-
-      vim.b[ev.buf].osc7_dir = dir
-      if vim.api.nvim_get_current_buf() == ev.buf then
-        vim.cmd.lcd(dir)
+      local val, n = string.gsub(ev.data.sequence, "\027]7;file://[^/]*", "")
+      if n > 0 then
+        -- OSC 7: dir-change
+        local dir = val
+        if vim.fn.isdirectory(dir) == 0 then
+          vim.notify(string.format("invalid dir: %s, uri: %s", dir, uri))
+          return
+        end
+        vim.cmd.bcd(dir)
       end
     end,
   })
