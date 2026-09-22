edc5c3e8742f14d3e5aec125c07928cc96223c49
hat0uma <55551571+hat0uma@users.noreply.github.com>
2026-09-22 22:14:32 +0900

    update

--- .config/aconfmgr/33-packages-sdr.sh
+++ .config/aconfmgr/33-packages-sdr.sh
@@ -1,3 +1,4 @@
 AddPackage gnuradio           # Signal processing runtime and signal processing software development toolkit
 AddPackage gnuradio-companion # Signal processing runtime and signal processing software development toolkit (GUI)
 AddPackage urh                # Universal Radio Hacker: investigate wireless protocols like a boss
+AddPackage hackrf             # Driver for HackRF, allowing general purpose software defined radio (SDR)
